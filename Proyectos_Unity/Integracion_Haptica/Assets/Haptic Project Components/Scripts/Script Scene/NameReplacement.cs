df0ec61a0d7445a54371fabca9394d627a1bc8f1
JuanSMartinez <js.martinez777@uniandes.edu.co>
2016-09-14 11:47:40 -0500

    solucion problema de nombres

---
+++ Proyectos_Unity/Integracion_Haptica/Assets/Haptic Project Components/Scripts/Script Scene/NameReplacement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NameReplacement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string name_child = gameObject.name;
+        gameObject.GetComponentInChildren<MeshFilter>().mesh.name = name_child;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
