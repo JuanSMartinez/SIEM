ea7cf2f859fbdccb9ff1a072120e6839234eb40e
JuanSMartinez <js.martinez777@uniandes.edu.co>
2016-09-27 11:16:37 -0500

    adicion de objetos y cambio de script principal

---
+++ Proyectos_Unity/Colliders_Clavicula/Assets/Scripts/MeshName.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshName : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Mesh mesh = gameObject.GetComponentInChildren<MeshFilter>().mesh;
+        Debug.Log("Mesh name: " + mesh.name);
+        mesh.name = "Pancrasio";
+        Debug.Log("Mesh name pancrazised: " + mesh.name);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
