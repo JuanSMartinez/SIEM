1b778c77473a4ba165f7b6d9b9e6a35bd4949906
JuanSMartinez <js.martinez777@uniandes.edu.co>
2016-10-04 22:08:26 -0500

    proyecto de prueba bounds

---
+++ Proyectos_Unity/Bounds_Manager/Assets/Scripts/CheckBounds.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckBounds : MonoBehaviour {
+
+	public float speed = 2.0f;
+
+	//Initial forward position
+	private Vector3 initial;
+
+	// Use this for initialization
+	void Start () {
+		//Get initial transform of the object
+		initial = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow)){
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			transform.position += Vector3.left* speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.UpArrow)){
+			transform.position += Vector3.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)){
+			transform.position += Vector3.back* speed * Time.deltaTime;
+		}
+		checkBounds ();
+	}
+
+	//Check bounds and alert
+	private void checkBounds(){
+		Vector3 actualPosition = gameObject.transform.position;
+		float diff = Mathf.Abs (Vector3.Distance (actualPosition, initial));
+		Debug.Log ("Inicial: " + initial);
+		Debug.Log ("Actual: " + actualPosition);
+		Debug.Log ("Diff: " + diff);
+	}
+}
