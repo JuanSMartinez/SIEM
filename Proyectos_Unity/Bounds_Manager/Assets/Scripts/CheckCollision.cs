1f61ab7cfdbfaeb8e8f7cbbcfa02a98eb238d88c
JuanSMartinez <js.martinez777@uniandes.edu.co>
2016-10-10 09:39:25 -0500

    Check collisions

---
+++ Proyectos_Unity/Bounds_Manager/Assets/Scripts/CheckCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision){
+		Debug.Log ("Collided with " + collision.gameObject.name);
+	}
+
+	void OnCollisionExit(Collision collisionInfo) {
+		Debug.Log ("Stop colliding with " + collisionInfo.gameObject.name);
+	}
+}
