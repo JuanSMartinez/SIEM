37624362a72f20e9f3dfaf0fb83b41bf037b5e0b
JuanSMartinez <js.martinez777@uniandes.edu.co>
2016-10-25 17:39:34 -0500

    Musculo hueco

--- Proyectos_Unity/Simulador/Assets/Scripts/Surgery/Simulation/AcnhorMovement.cs
+++ Proyectos_Unity/Simulador/Assets/Scripts/Surgery/Simulation/AcnhorMovement.cs
@@ -5,9 +5,29 @@ public class AcnhorMovement : MonoBehaviour {
 
 	public GameObject A;
 	public GameObject B;
+	public GameObject[] cursorObjects;
+	public Material staticMaterial;
+	public Material anchorMaterial;
 	private bool movement = false;
 
 	void Update(){
+		if(PluginImport.GetButton2State())
+			movement = !movement;
+
+		SetMaterial ();
+		ModifyJoints ();
+		
+
+	}
+
+	private void SetMaterial (){
+		for (int i = 0; i < cursorObjects.Length; i++) {
+			cursorObjects[i].GetComponent <MeshRenderer> ().material = movement ? anchorMaterial: staticMaterial;
+		}
+
+	}
+
+	private void ModifyJoints(){
 		if (movement) {
 			ConfigurableJoint[] joints = A.GetComponents<ConfigurableJoint> ();
 			for (int i = 0; i < joints.Length; i++) {
@@ -18,10 +38,6 @@ public class AcnhorMovement : MonoBehaviour {
 				joints [i].anchor = A.transform.position;
 			}
 		}
-	}
-
-	public void MoveAnchors(){
-		movement = !movement;
 	}
 
 }
--- Proyectos_Unity/Simulador/Assets/Scripts/Surgery/Simulation/SceneSelection.cs
+++ Proyectos_Unity/Simulador/Assets/Scripts/Surgery/Simulation/SceneSelection.cs
@@ -8,8 +8,14 @@ public class SceneSelection : MonoBehaviour {
 
 	// Scene 2
 	public void Scene2(){
-		Debug.Log ("Second");
+		
 		esternon.transform.Rotate(new Vector3(0f, 5f,0f));
 		acromion.transform.Rotate(new Vector3(0f, 5f,0f));
 	}
+
+	public void Scene3(){
+		esternon.transform.Rotate(new Vector3(0f, 5f,0f));
+		acromion.transform.Rotate(new Vector3(0f, 5f,0f));
+	}
+
 }
