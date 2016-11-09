7f03f17a729fc25f34eeeb3b7f15ac57e917b3ac
JuanSMartinez <js.martinez777@uniandes.edu.co>
2016-11-09 12:18:50 -0500

    avance en implementacion de monitor

--- Proyectos_Unity/Simulador_2.0/Assets/Scripts/Simulation/Model/Monitor.cs
+++ Proyectos_Unity/Simulador_2.0/Assets/Scripts/Simulation/Model/Monitor.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Monitor : MonoBehaviour {
 
@@ -9,6 +10,12 @@ public class Monitor : MonoBehaviour {
 	//Control for collision guides
 	public bool collisionGuides = false;
 
+	//Objets to track
+	public GameObject[] objects;
+
+	//Final objects to compare to
+	public GameObject[] finalObjects;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -16,6 +23,17 @@ public class Monitor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		//Show final positions if the guide options is enabled
+		ShowFinalGuides();
+	}
+
+	//Show final positions if the guide options is enabled
+	private void ShowFinalGuides(){
+		for (int i = 0; i < finalObjects.Length; i++) {
+			GameObject obj = finalObjects [i];
+			obj.transform.GetComponentInChildren<MeshRenderer> ().enabled = positionGuides;
+		}
+		
 	}
 }
