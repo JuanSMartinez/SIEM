023b0a76b2ca65cd726e7714d2ad0ee651e1b07e
JuanSMartinez <js.martinez777@uniandes.edu.co>
2016-11-10 08:40:06 -0500

    UI en una escena

--- Proyectos_Unity/Simulador_2.0/Assets/Scripts/Simulation/Model/Monitor.cs
+++ Proyectos_Unity/Simulador_2.0/Assets/Scripts/Simulation/Model/Monitor.cs
@@ -16,6 +16,12 @@ public class Monitor : MonoBehaviour {
 	//Final objects to compare to
 	public GameObject[] finalObjects;
 
+	//Linear tolerance
+	public float linearTolerance;
+
+	//Angular tolerance
+	public float angularTolerance;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -26,6 +32,10 @@ public class Monitor : MonoBehaviour {
 
 		//Show final positions if the guide options is enabled
 		ShowFinalGuides();
+
+		//Log position differences
+
+		//Log angular differences
 	}
 
 	//Show final positions if the guide options is enabled
@@ -35,5 +45,49 @@ public class Monitor : MonoBehaviour {
 			obj.transform.GetComponentInChildren<MeshRenderer> ().enabled = positionGuides;
 		}
 		
+	}
+
+	//Calculate overall difference of positions between an object and its desired position
+	private float GetOverallDistanceDiff (GameObject obj, GameObject target){
+		//Raw difference between the positions
+		float difference = Vector3.Distance(obj.transform.position, target.transform.position);
+		return difference;
+	}
+
+	//Calculate the distance difference for each coordinate between an object and its desired position
+	//Returns the differences in an vector 3 object
+	private Vector3 GetCoordinateDistanceDiff(GameObject obj, GameObject target){
+		//X difference
+		float xdiff = Mathf.Abs(obj.transform.position.x - target.transform.position.x);
+
+		//Y difference
+		float ydiff = Mathf.Abs(obj.transform.position.y - target.transform.position.y);
+
+		//Z difference
+		float zdiff = Mathf.Abs(obj.transform.position.z - target.transform.position.z);
+
+		return new Vector3 (xdiff, ydiff, zdiff);
+	}
+
+	//Calculate overall difference of angles between an object and its desired position
+	private float GetOverallAngleDiff(GameObject obj, GameObject target){
+		//Raw difference between angles of position vectors
+		float difference = Vector3.Distance(obj.transform.eulerAngles, target.transform.eulerAngles);
+		return difference;
+	}
+
+	//Calculate the angular difference for each coordinate between an object and its desired position
+	//Returns the differences in an vector 3 object
+	private Vector3 GetCoordinateAngleDiff(GameObject obj, GameObject target){
+		//X difference
+		float xdiff = Mathf.Abs(obj.transform.eulerAngles.x - target.transform.eulerAngles.x);
+
+		//Y difference
+		float ydiff = Mathf.Abs(obj.transform.eulerAngles.y - target.transform.eulerAngles.y);
+
+		//Z difference
+		float zdiff = Mathf.Abs(obj.transform.eulerAngles.z - target.transform.eulerAngles.z);
+
+		return new Vector3 (xdiff, ydiff, zdiff);
 	}
 }
